f5fdb935d14ca3e3eff03835f38d26fa01da2c96
Floemia <mcescobar73@gmail.com>
2024-09-25 14:01:31 -0400

    changed droid tracking loop

--- src/functions/osu!droid/embeds.ts
+++ src/functions/osu!droid/embeds.ts
@@ -10,11 +10,11 @@ const score = async (recent: DroidScoreScraped) => {
     const embed = new EmbedBuilder()
     if (!recent.beatmap) {
         embed.setAuthor({name: recent.fallback_title, iconURL: recent.user.avatar_url})
-    } else { embed.setAuthor({name: `${recent.beatmap.artist} - ${recent.beatmap.title} [${recent.beatmap.version}] ${recent.performance.stars_dr? `${recent.performance.stars_dr.toFixed(2)} ⭐`: ''} +${mods.str} ${mods.speed != 1 ? `(${mods.speed.toFixed(2)}x)` : ``} `, iconURL: recent.user.avatar_url, url: `https://osu.ppy.sh/beatmapsets/${recent.beatmap.beatmapsetID}#osu/${recent.beatmap.beatmapID}`})}
+    } else { embed.setAuthor({name: `${recent.beatmap.artist} - ${recent.beatmap.title} [${recent.beatmap.version}] ${recent.performance.stars_pc ? `${recent.performance.stars_pc.toFixed(2)} ⭐`: ''} ${mods.str? `+ ${mods.str}` : ''} ${mods.speed != 1 ? `(${mods.speed.toFixed(2)}x)` : ``} `, iconURL: recent.user.avatar_url, url: `https://osu.ppy.sh/beatmapsets/${recent.beatmap.beatmapsetID}#osu/${recent.beatmap.beatmapID}`})}
     embed.setDescription(`> ${rank} | **${recent.scraped_pp ? recent.scraped_pp.toFixed(2) : `...`} DPP  ─  ${recent.performance.pp ? `${recent.performance.pp?.toFixed(2)}` : '...'} PP**\n> ${recent.score.toLocaleString("en-US")} | ${recent.accuracy.toFixed(2)}% | ${recent.combo.toLocaleString("en-US")}x${recent.beatmap? ` / ${recent.beatmap.maxCombo.toLocaleString("en-US")}x`: ''}`)
     embed.setFooter({ text: `${client.user.username}`, iconURL: client.user.displayAvatarURL({ extension: "png" }) })
     embed.setColor(Number(`0x${recent.embed_color?.slice(1)}`))
-    embed.setTimestamp(recent.timestamp)
+    embed.setTimestamp(recent.timestamp - 7200000)
     if (recent.embed_color != "#dedede"){
         embed.setImage(`https://assets.ppy.sh/beatmaps/${recent.beatmap?.beatmapsetID}/covers/cover.jpg`)
     }
--- src/handlers/tracking.ts
+++ src/handlers/tracking.ts
@@ -9,54 +9,47 @@ import GuildConfigModel from "../schemas/guild"
 export const droid_tracking = async () => {
 
     var tracking_users = await DroidAccountTrackModel.find()
-    setInterval(async () => {
-       tracking_users = await DroidAccountTrackModel.find()
-    },60000)
 
     if (tracking_users) {
-        setInterval(async () => {
-            tracking_users.forEach(async (user, i) => {
-                setTimeout(async () => {
-                    const track_channel =  client.channels.cache.get(`${(await GuildConfigModel.findOne({ id: user.guild }))?.channel.track}`)
-                    if (!track_channel || track_channel.type != ChannelType.GuildText) return
+        while (true) {
+            tracking_users = await DroidAccountTrackModel.find()
+            for await (const user of tracking_users) {
+                await new Promise(resolve => setTimeout(resolve, 25000))
 
-                    const recents = await droid.recent(user.uid)
-                    if (!recents) return
-                    if (!recents[0]) return
-                    if (recents[0].timestamp == user.timestamp) return
-                    
-                    const play = recents[0]
-                    const beatmap = await MapInfo.getInformation(play.hash)
-                    console.log(`creando track para ${play.user.username}\n${play.fallback_title}\n\n`)
-                    
-                    if (beatmap?.title) {
-                        play.beatmap = beatmap
+                const track_channel = client.channels.cache.get(`${(await GuildConfigModel.findOne({ id: user.guild }))?.channel.track}`)
+                if (!track_channel || track_channel.type != ChannelType.GuildText) return
 
-                        try {
-                            const average = await getAverageColor(`https://assets.ppy.sh/beatmaps/${play.beatmap?.beatmapsetID}/covers/cover.jpg`)
-                            play.embed_color = average.hex
-                          } catch (error) {
-                            console.log(`invalid beatmap / no bg ${play.beatmap?.beatmapsetID} `)
-                          }
+                const recents = await droid.recent(user.uid)
+                if (!recents) return
+                if (!recents[0]) return
+                if (recents[0].timestamp == user.timestamp) return
 
-                        const calc = await droid.calculate(play)
-                        play.performance = calc.performance
-                        play.performance_fc = calc.performance_fc
+                const play = recents[0]
+                const beatmap = await MapInfo.getInformation(play.hash)
+
+                await DroidAccountTrackModel.findOneAndUpdate({ uid: play.user.id }, {
+                    timestamp: play.timestamp,
+                    last_score: play.score
+                })
+
+                console.log(`creando track para ${play.user.username}\n${play.fallback_title}\n${play.accuracy}, ${play.mods}, ${play.scraped_pp}dpp\n`)
+
+                if (beatmap?.title) {
+                    play.beatmap = beatmap
+                    try {
+                        const average = await getAverageColor(`https://assets.ppy.sh/beatmaps/${play.beatmap?.beatmapsetID}/covers/cover.jpg`)
+                        play.embed_color = average.hex
+                    } catch (error) {
+                        console.log(`invalid beatmap / no bg ${play.beatmap?.beatmapsetID} `)
                     }
-                    const embed = await droid.embed.score(play)
-                    track_channel.send({content: `<:droid_simple:1021473577951821824>  **osu!droid** | Score reciente de  **:flag_${user.country.toLowerCase()}:  ${user.username}**\n-# Los valores de DPP y PP pueden no ser precisos.`,embeds: [embed]})
-                    user.timestamp = play.timestamp
-                    user.last_score = play.score
 
-                    await DroidAccountTrackModel.findOneAndUpdate({uid: play.user.id}, {
-                        timestamp: play.timestamp,
-                        last_score: play.score
-                    })
-
-                    
-                }, (i * 4000) + 4000 * i)
-            }, )
-
-        }, (tracking_users.length * 5000) + 5000 * tracking_users.length + 5000)
+                    const calc = await droid.calculate(play)
+                    play.performance = calc.performance
+                    play.performance_fc = calc.performance_fc
+                }
+                const embed = await droid.embed.score(play)
+                track_channel.send({ content: `<:droid_simple:1021473577951821824>  **osu!droid** | Score reciente de  **:flag_${user.country.toLowerCase()}:  ${user.username}**\n-# Los valores de DPP y PP pueden no ser precisos.`, embeds: [embed] })
+            }
+        }
     }
 }
