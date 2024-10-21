5434ad792d3045699bfe17b8c3c0d72f517841db
Floemia <mcescobar73@gmail.com>
2024-10-21 19:25:42 -0300

    added pfp and presence

---
+++ src/handlers/presence.ts
@@ -0,0 +1,50 @@
+
+import { ActivityType, PresenceData } from "discord.js"
+import * as fs from "fs/promises"
+import { GlobClient } from "../types";
+export const presence_loop = async (client: GlobClient) => {
+	function random(min: number, max:number ) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	const activities: PresenceData[] = [
+		{ activities: [{ name: `bangers`, type: ActivityType.Listening }], status: "idle"},
+		{ activities: [{ name: `osu!droid`, type: ActivityType.Playing }], status: "dnd"},
+		{ activities: [{ name: `Michi's Gang`, type: ActivityType.Watching }], status: "online"},
+		{ activities: [{ name: `osu!`, type: ActivityType.Competing }], status: "dnd"}
+	]
+
+	let current_act: PresenceData
+	let current_pfp: string
+	let act = activities
+	let pfp = await fs.readdir("./assets/avatar")
+	const activity_interval = () => {
+		if (act.length == 0) act = activities
+		let index = Math.floor( Math.random() * act.length )
+
+		while( act[ index ] == current_act){
+			index = Math.floor( Math.random() * act.length )
+		}
+		current_act = act[index]
+		client.user.setPresence(current_act)
+		act = act.filter( activity => activity != act[index])
+	}
+
+	const pfp_interval = async () => {
+		if (pfp.length == 0) pfp = await fs.readdir("./assets/avatar")
+		let index = Math.floor( Math.random() * pfp.length )
+		while( pfp[ index ] == current_pfp){
+			index = Math.floor( Math.random() * pfp.length )
+		}
+		current_pfp = pfp[index]
+		await client.user.setAvatar(`./assets/avatar/${pfp[index]}`)
+		act = act.filter( pic => pic != pfp[index])
+	}
+
+	activity_interval()
+	// minutes
+	setInterval(activity_interval, 5 * 60000);
+	setInterval( async () => await pfp_interval(), 60 * 60000)
+
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config"
 import { Client, Collection } from "discord.js"
 import type { GlobClient } from "./types"
 import { handleEvents } from "./handlers/events"
+import { presence_loop } from "./handlers/presence"
 import { osu_api_connect } from "./handlers/osu_api"
 import { connect_mongoose } from "./handlers/mongoose"
 import Logger, { LoggerOptions } from "@ptkdev/logger"
@@ -15,9 +16,7 @@ client.commands = new Collection()
 handleEvents(client)
 client.login(process.env.TOKEN)
 
-osu_api_connect()
 connect_mongoose()
-
 const options: LoggerOptions = {
 	language: "en",
 	colors: true,
@@ -41,4 +40,7 @@ const options: LoggerOptions = {
 
 export const logger = new Logger(options)
 
+client.on("ready", async () => {
+	await presence_loop(client)
+})
 
