194e0a917e9b788c0a385713f5fb82ddb9df15cf
Floemia <mcescobar73@gmail.com>
2024-10-22 21:33:23 -0300

    .

--- src/handlers/mongoose.ts
+++ src/handlers/mongoose.ts
@@ -5,14 +5,14 @@ export const connect_mongoose = async () => {
 	const MONGO_URI = process.env.MONGO_URI
 	if (!MONGO_URI) return logger.warning("MongoDB URI not found.", "DATABASE")
 
-	mongoose.connect(`${MONGO_URI}`)
+	await mongoose.connect(`${MONGO_URI}`)
 		.then(async () => {
 			logger.info("MongoDB connection established.", "DATABASE")
-			if (process.env.DROID_TRACKING_ENABLED == "true") {
-				await droid_tracking()
-			}
 			if (process.env.OSU_TRACKING_ENABLED == "true") {
 				await osu_tracking()
+			}
+			if (process.env.DROID_TRACKING_ENABLED == "true") {
+				await droid_tracking()
 			}
 		})
 		.catch((error) => logger.error(`An error occurred while connecting to MongoDB - ${error}`, "DATABASE"))
