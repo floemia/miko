bcd35cfe3f0304cd612076842c5992abd45e743c
floemia <mcescobar73@gmail.com>
2025-07-12 20:38:00 -0300

    v3.1.5 - Using updated @floemia/osu-droid-card

--- src/commands/osu!droid/card/card.ts
+++ src/commands/osu!droid/card/card.ts
@@ -2,10 +2,9 @@ import { SlashCommand } from "@structures/core";
 import { Droid, Embeds } from "@utils";
 import { AttachmentBuilder, EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import { en, es } from "@locales";
-import { card } from "osu-droid-card";
 import { DroidBanchoUser, DroidRXUser } from "miko-modules";
+import { DroidCard } from "@floemia/osu-droid-card";
 
-export const disabled: SlashCommand["disabled"] = true;
 export const run: SlashCommand["run"] = async (client, interaction) => {
 	await interaction.deferReply();
 	const spanish = interaction.locale.includes("es");
@@ -22,7 +21,7 @@ export const run: SlashCommand["run"] = async (client, interaction) => {
 
 	const embed_wait = Embeds.process(str.commands.card.generating(user));
 	const response = await interaction.editReply({ embeds: [embed_wait] });
-	const card_data = await card({ uid: user.id });
+	const card_data = await DroidCard.create(user);
 	const filename = `${user.username}-${user.id}.png`;
 	const attachment = new AttachmentBuilder(card_data!, { name: filename });
 	const embed = new EmbedBuilder()
@@ -30,14 +29,14 @@ export const run: SlashCommand["run"] = async (client, interaction) => {
 		.setColor(Number(`0x${user.color.slice(1)}`))
 		.setAuthor({ name: interaction.user.displayName, iconURL: interaction.user.displayAvatarURL() })
 		.setTimestamp()
-		.setFooter({ text: client.user?.displayName!, iconURL: client.user?.displayAvatarURL() });
+		.setFooter({ text: client.user?.displayName! + " - Beta"});
 	await response.edit({ embeds: [embed], files: [attachment] });
 }
 export const data: SlashCommand["data"] =
 	new SlashCommandBuilder()
 		.setName("card")
-		.setDescription("🔘 (iBancho) Get a profile card from yourself or a player.")
-		.setDescriptionLocalization("es-ES", "🔘 (iBancho) Obtener una tarjeta de perfil tuya o de un jugador.")
+		.setDescription("🔘 (BETA!) Get a profile card from yourself or a player.")
+		.setDescriptionLocalization("es-ES", "🔘 (BETA!) Obtener una tarjeta de perfil tuya o de un jugador.")
 		.addUserOption(option => option.setName("user")
 			.setDescription("The Discord user linked to the osu!droid account.")
 			.setDescriptionLocalization("es-ES", "El usuario de Discord vinculado a la cuenta de osu!droid."))
